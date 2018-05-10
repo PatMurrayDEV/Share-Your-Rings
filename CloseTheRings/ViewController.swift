882b12aa1d93bbac5ace5dedfac208b390cf90cd
Patrick Murray <patrick@patmurraydev.com>
2018-05-10 17:34:17 +1000

    Initial Commit

---
+++ CloseTheRings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloseTheRings
+//
+//  Created by Patrick Murray on 10/5/18.
+//  Copyright © 2018 Patrick Murray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
